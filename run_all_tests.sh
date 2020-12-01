d1f7b4f77867f29682cdac8f16842a93f9b276d0
Pascal Pieper <ppieper@tzi.de>
2020-12-01 11:59:31 +0100

    Yooo

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-buildfolder="../build"
-tests=("test_plic" "test_sensor" "test_sensor2")
-today=date +"%d-%m-%Y"
+buildfolder="build"
+tests=("test_sensor" "test_sensor2" "test_plic")
+today=$(date +"%Y-%m-%d.%H-%m")
+
+echo "Today is $today"
 mkdir test 2> /dev/null
+#	rm -r "$testfolder/*" 2> /dev/null
 
 for test in ${tests[@]}
 do
 	echo "Running test $test"
 	testfolder="test/$test"
 	mkdir $testfolder 2> /dev/null
-	rm -r "$testfolder/*" 2> /dev/null
 	make -C $buildfolder $test > "$testfolder/$today.log" 2>&1
+	mkdir "$testfolder/$today"
+	cp -rL $buildfolder/klee-last/* $testfolder/$today
 done
