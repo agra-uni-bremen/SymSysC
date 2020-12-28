739535d9e30edc1eaf3062297728d636eef57be1
Pascal Pieper <ppieper@tzi.de>
2020-12-28 13:22:36 +0100

    better date, better cmake

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 buildfolder="build"
 tests=("test_sensor" "test_sensor2" "test_uart" "test_plic")
-today=$(date +"%Y-%m-%d.%H-%m")
+today=$(date +"%Y-%m-%d-%H.%M")
 
 echo "Today is $today"
 mkdir test 2> /dev/null
