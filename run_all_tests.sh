3cd6bd7285af0f2b99df0bed641c53ab4054f5a9
Pascal Pieper <ppieper@uni-bremen.de>
2021-01-04 15:38:30 +0100

    Dividing subtest for increased shadyness

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 buildfolder="build"
-tests=("test_sensor" "test_sensor2" "test_uart" "test_plic")
+tests=("sensor" "sensor2" "uart" "plic#1" "plic#2")
 today=$(date +"%Y-%m-%d-%H.%M")
+klee_args=(
+    "--libcxx"
+    "--libc=uclibc"
+    "-posix-runtime"
+    #"--emit-all-errors=1"
+    )
 
 echo "Today is $today"
 mkdir test 2> /dev/null
 #	rm -r "$testfolder/*" 2> /dev/null
 
+make -C $buildfolder clean
 for test in ${tests[@]}
 do
-	echo "Running test $test"
-	testfolder="test/$test"
-	mkdir $testfolder 2> /dev/null
-	make -C $buildfolder $test > "$testfolder/$today.log" 2>&1
+	base_name=$(echo $test | cut -d "#" -f1)
+	subtype=$(echo $test | cut -d "#" -f2)
+	testfolder=test/$test
+	echo "Running test $base_name ($subtype)"
+	mkdir "$testfolder" 2> /dev/null
+	make -C $buildfolder testbench_$base_name
+	{ time klee ${klee_args[*]} $buildfolder/testbench_$base_name $subtype ; } > "$testfolder/$today.log" 2>&1
 	mkdir "$testfolder/$today"
-	cp -rL $buildfolder/klee-last/* $testfolder/$today
+	cp -rL $buildfolder/klee-last/* "$testfolder/$today"
 done
