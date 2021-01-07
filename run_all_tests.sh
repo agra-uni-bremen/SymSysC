05ee0ef3dff81f2e7b7754be0528c1a2021c59b7
Pascal Pieper <ppieper@tzi.de>
2021-01-07 12:25:02 +0100

    Better style, more RAM

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,31 +2,33 @@
 buildfolder="build"
 tests=("sensor" "sensor2" "uart" "plic#1" "plic#2" "plic#3")
 today=$(date +"%Y-%m-%d-%H.%M")
+testfolder_base=test/$today
 klee_args=(
     "--libcxx"
     "--libc=uclibc"
     "-posix-runtime"
+    "-max-memory=40000"	#default: 2000 -> 2GB
     #"--emit-all-errors=1"
     )
 
-echo "Today is $today"
-mkdir test 2> /dev/null
+echo "Today is $today, writing to $testfolder_base"
 #	rm -r "$testfolder/*" 2> /dev/null
+mkdir -p $testfolder_base 2> /dev/null
 
 make -s -C $buildfolder clean
 for test in ${tests[@]}
 do
 	base_name=$(echo $test | cut -d "#" -f1)
-	subtype=$(echo $test | cut -d "#" -f2)
-	testfolder=test/$test
+	subtype=$(echo $test | cut -d "#" -s -f2)
+	testfolder=$testfolder_base/$test
 	echo "Running test $base_name ($subtype)"
 	mkdir "$testfolder" 2> /dev/null
 	make -C $buildfolder testbench_$base_name --no-print-directory
-	{ time klee ${klee_args[*]} $buildfolder/testbench_$base_name $subtype ; } > "$testfolder/$today.log" 2>&1
+	{ time klee ${klee_args[*]} $buildfolder/testbench_$base_name $subtype ; } > "$testfolder/run.log" 2>&1
 	mkdir "$testfolder/$today"
 	cp -rL $buildfolder/klee-last/* "$testfolder/$today"
 done
-klee-stats test/*/$today > test/$today.klee-stat.log
-tail test/*/$today.log >> test/$today.klee-stat.log
+klee-stats $testfolder_base/* > $testfolder_base/klee-stat.log
+tail $testfolder_base/*/run.log >> $testfolder_base/klee-stat.log
 
-cat test/$today.klee-stat.log
+cat $testfolder_base/klee-stat.log
