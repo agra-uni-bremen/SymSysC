9ab6f5b449c834584de8287b1e6f8395ea5e8bd7
Pascal Pieper <ppieper@tzi.de>
2021-01-21 15:55:13 +0100

    Yeah, better now

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -17,7 +17,7 @@ klee_args=(
     "--libcxx"
     "--libc=uclibc"
     "-posix-runtime"
-    "-max-memory=40000"	#default: 2000 -> 2GB
+    #"-max-memory=40000"	#default: 2000 -> 2GB
     #"--emit-all-errors=1"
     )
 
@@ -37,24 +37,22 @@ do
 	make -C $buildfolder testbench_$base_name --no-print-directory
 	echo "Running test $base_name ($subtype)"
 	{ time klee ${klee_args[*]} $buildfolder/testbench_$base_name $subtype ; } > "$testfolder/run.log" 2>&1 &
-	klee_pid[$i]=$!
-	echo "$base_name ($subtype) running as $klee_pid[$test]"
+	klee_pid[${i}]=$!
 	sleep 1
-	klee_folder[$i]=$(readlink -f $buildfolder/klee-last)
-	klee_target_folder[$i]=$testfolder
-	$i=$[i + 1]
+	klee_folder[${i}]=$(readlink -f $buildfolder/klee-last)
+	klee_target_folder[${i}]=$testfolder
+	echo "$base_name ($subtype) running as ${klee_pid[${i}]} into ${klee_folder[${i}]}"
+	i=$[i + 1]
 done
 
 echo "All processes started. Waiting for processes to finish..."
 
-for i in {0 ..${#tests[@]}}
-do
-    wait klee_pid[$i]
-    echo "$tests[$i] finished."
-    mv "$klee_folder[$i]" "$klee_target_folder[$i]"
+for ((i=0;i<${#tests[@]};i++)); do
+    echo "waiting for PID ${klee_pid[${i}]}..."
+    wait "${klee_pid[${i}]}"
+    echo "${tests[${i}]} finished."
+    mv "${klee_folder[${i}]}" "${klee_target_folder[${i}]}"
 done
-
-
 
 klee-stats $testfolder_base/* > $testfolder_base/klee-stat.log
 tail $testfolder_base/*/run.log >> $testfolder_base/klee-stat.log
