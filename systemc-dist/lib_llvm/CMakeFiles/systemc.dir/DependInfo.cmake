f5ad78918645a5993d61303f2f599716f1d479bc
Pascal Pieper <ppieper@tzi.de>
2021-03-08 16:59:03 +0100

    It builds in bytecode, at least

---
+++ systemc-dist/lib_llvm/CMakeFiles/systemc.dir/DependInfo.cmake
@@ -0,0 +1,123 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_clock.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_clock.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_event_finder.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_event_finder.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_event_queue.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_event_queue.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_export.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_export.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_interface.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_interface.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_mutex.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_mutex.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_port.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_port.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_prim_channel.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_prim_channel.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_semaphore.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_semaphore.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_signal.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_signal.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_signal_ports.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_signal_ports.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_signal_resolved.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_signal_resolved.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/communication/sc_signal_resolved_ports.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/communication/sc_signal_resolved_ports.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/bit/sc_bit.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/bit/sc_bit.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/bit/sc_bv_base.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/bit/sc_bv_base.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/bit/sc_logic.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/bit/sc_logic.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/bit/sc_lv_base.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/bit/sc_lv_base.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/sc_fxcast_switch.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/sc_fxcast_switch.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/sc_fxdefs.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/sc_fxdefs.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/sc_fxnum.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/sc_fxnum.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/sc_fxnum_observer.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/sc_fxnum_observer.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/sc_fxtype_params.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/sc_fxtype_params.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/sc_fxval.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/sc_fxval.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/sc_fxval_observer.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/sc_fxval_observer.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/scfx_mant.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/scfx_mant.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/scfx_pow10.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/scfx_pow10.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/scfx_rep.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/scfx_rep.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/fx/scfx_utils.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/fx/scfx_utils.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_int_base.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_int_base.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_int_mask.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_int_mask.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_length_param.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_length_param.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_nbexterns.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_nbexterns.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_nbutils.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_nbutils.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_signed.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_signed.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_uint_base.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_uint_base.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/int/sc_unsigned.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/int/sc_unsigned.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/misc/sc_concatref.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/misc/sc_concatref.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/datatypes/misc/sc_value_base.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/datatypes/misc/sc_value_base.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_attribute.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_attribute.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_cor_fiber.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_cor_fiber.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_cor_pthread.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_cor_pthread.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_cor_qt.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_cor_qt.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_cthread_process.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_cthread_process.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_event.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_event.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_except.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_except.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_join.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_join.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_main.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_main.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_main_main.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_main_main.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_method_process.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_method_process.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_module.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_module.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_module_name.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_module_name.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_module_registry.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_module_registry.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_name_gen.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_name_gen.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_object.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_object.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_object_manager.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_object_manager.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_phase_callback_registry.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_phase_callback_registry.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_process.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_process.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_reset.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_reset.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_sensitive.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_sensitive.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_simcontext.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_simcontext.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_spawn_options.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_spawn_options.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_thread_process.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_thread_process.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_time.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_time.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_ver.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_ver.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_wait.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_wait.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/kernel/sc_wait_cthread.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/kernel/sc_wait_cthread.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/tracing/sc_trace.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/tracing/sc_trace.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/tracing/sc_trace_file_base.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/tracing/sc_trace_file_base.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/tracing/sc_vcd_trace.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/tracing/sc_vcd_trace.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/tracing/sc_wif_trace.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/tracing/sc_wif_trace.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_hash.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_hash.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_list.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_list.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_mempool.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_mempool.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_pq.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_pq.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_report.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_report.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_report_handler.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_report_handler.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_stop_here.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_stop_here.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_string.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_string.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_utils_ids.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_utils_ids.cpp.o"
+  "/home/klee/systemc-2.3.3/src/sysc/utils/sc_vector.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/sysc/utils/sc_vector.cpp.o"
+  "/home/klee/systemc-2.3.3/src/tlm_core/tlm_2/tlm_generic_payload/tlm_gp.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/tlm_core/tlm_2/tlm_generic_payload/tlm_gp.cpp.o"
+  "/home/klee/systemc-2.3.3/src/tlm_core/tlm_2/tlm_generic_payload/tlm_phase.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/tlm_core/tlm_2/tlm_generic_payload/tlm_phase.cpp.o"
+  "/home/klee/systemc-2.3.3/src/tlm_core/tlm_2/tlm_quantum/tlm_global_quantum.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/tlm_core/tlm_2/tlm_quantum/tlm_global_quantum.cpp.o"
+  "/home/klee/systemc-2.3.3/src/tlm_utils/convenience_socket_bases.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/tlm_utils/convenience_socket_bases.cpp.o"
+  "/home/klee/systemc-2.3.3/src/tlm_utils/instance_specific_extensions.cpp" "/home/klee/systemc-2.3.3/build/src/CMakeFiles/systemc.dir/tlm_utils/instance_specific_extensions.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SC_BUILD"
+  "SC_ENABLE_ASSERTIONS"
+  "SC_ENABLE_EARLY_MAXTIME_CREATION"
+  "SC_ENABLE_SIMULATION_PHASE_CALLBACKS_TRACING"
+  "SC_INCLUDE_FX"
+  "SC_USE_PTHREADS"
+  "systemc_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/klee/systemc-2.3.3/build/src/libsystemc.so" "/home/klee/systemc-2.3.3/build/src/libsystemc.so.2.3.3"
+  "/home/klee/systemc-2.3.3/build/src/libsystemc.so.2.3" "/home/klee/systemc-2.3.3/build/src/libsystemc.so.2.3.3"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
